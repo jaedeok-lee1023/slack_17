751cb4c755479dca665f1ab82c45e17431e6691c
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2025-04-10 10:56:50 +0900

    Create kurly.py

---
+++ kurly.py
@@ -0,0 +1,9 @@
+class KurlyCluster:
+    def __init__(self, channel, name):
+        self.channel = channel
+        self.name = name
+
+# 컬리 클러스터 슬랙 채널 목록
+clusters = [
+    KurlyCluster('C08MGKRTX4M', '식수 테스트'),
+]
